348584f494730373fe785996d7cee1c30b9c0947
goody <goody@grimaceisnice.com>
2022-08-12 09:37:22 -0400

    fix dashes

--- terraform/tfstack_root_module_templates/s3_bucket/s3_bucket_variables.tf
+++ terraform/tfstack_root_module_templates/s3_bucket/s3_bucket_variables.tf
@@ -1,12 +1,12 @@
 variable "bucket_names" {
   description = "Bucket names per region. A good convention is <product>-<env>-<region>"
   default = {
-    goody-us-east-1 = "EXAMPLE_unique_bucket_name-goody--us-east-1"
-    goody-us-west-2 = "EXAMPLE_unique_bucket_name-goody--us-west-2"
-    qa-us-east-1    = "EXAMPLE_unique_bucket_name-qa--us-east-1"
-    qa-us-west-2    = "EXAMPLE_unique_bucket_name-qa--us-west-2"
-    prod-us-east-1  = "EXAMPLE_unique_bucket_name-prod--us-east-1"
-    prod-us-west-2  = "EXAMPLE_unique_bucket_name-prod--us-west-2"
+    goody-us-east-1 = "EXAMPLE_unique_bucket_name-goody-us-east-1"
+    goody-us-west-2 = "EXAMPLE_unique_bucket_name-goody-us-west-2"
+    qa-us-east-1    = "EXAMPLE_unique_bucket_name-qa-us-east-1"
+    qa-us-west-2    = "EXAMPLE_unique_bucket_name-qa-us-west-2"
+    prod-us-east-1  = "EXAMPLE_unique_bucket_name-prod-us-east-1"
+    prod-us-west-2  = "EXAMPLE_unique_bucket_name-prod-us-west-2"
   }
 }
 variable "versioning" {
